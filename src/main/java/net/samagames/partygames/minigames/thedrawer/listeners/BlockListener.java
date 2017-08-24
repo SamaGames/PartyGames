ed857a1470356ce5480e00b566474914e93cf726
BlueSlime <jeremylevilain78@orange.fr>
2017-08-24 13:55:37 +0200

    Publishing source code.

--- src/main/java/net/samagames/partygames/minigames/thedrawer/listeners/BlockListener.java
+++ src/main/java/net/samagames/partygames/minigames/thedrawer/listeners/BlockListener.java
@@ -11,6 +11,22 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 
+/*
+ * This file is part of PartyGames.
+ *
+ * PartyGames is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PartyGames is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PartyGames.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class BlockListener implements Listener {
 
     private TheDrawer miniGame;
--- src/main/java/net/samagames/partygames/minigames/villagerrun/entities/ai/PathfinderGoalWalk.java
+++ src/main/java/net/samagames/partygames/minigames/villagerrun/entities/ai/PathfinderGoalWalk.java
@@ -4,7 +4,22 @@ import net.minecraft.server.v1_9_R1.PathfinderGoal;
 import net.samagames.partygames.minigames.villagerrun.entities.NPC;
 import org.bukkit.Location;
 
-
+/*
+ * This file is part of PartyGames.
+ *
+ * PartyGames is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PartyGames is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PartyGames.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class PathfinderGoalWalk extends PathfinderGoal {
     /**
      * NPC's destination
